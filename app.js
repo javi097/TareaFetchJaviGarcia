2c4830706cacee343be1ce068b7d28ebcb37a548
JaviGarcia <55480199+javi097@users.noreply.github.com>
2020-01-25 20:44:34 +0100

    Parte 3
    Parte 3

--- app.js
+++ app.js
@@ -1,30 +1,47 @@
-window.addEventListener('laod',()=>{
+window.addEventListener("load", () => {
     let long;
     let lat;
     let temperatureDescription = document.querySelector(
         ".temperature-description"
     );
     let temperatureDegree = document.querySelector(".temperature-degree");
+    let locationTimezone = document.querySelector(".location-timezone");
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+
+
+            const proxy = "https://cors-anywhere.herokuapp.com/";
+            const api = `${proxy}https://api.darksky.net/forecast/58526512f8707a0b2fd39b66ae32cf35/${lat},${long}`;
+            
+            
+            
+            fetch(api)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+                const {
+                    temperature,
+                    summary,
+                    icon
+                } = data.currently;
+                //Set DOM Elements from the API
+
+                temperatureDegree.textContent = temperature;
+                temperatureDescription.textContent = summary;
+                locationTimezone.textContent = data.timezone;
+            });
+        });
+
+        
+    }
     
-    if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(position=>{
-            long= position.coords.longitude;
-            lat= position.coords.latitude;
-
-
-            const proxy='https://cors-anywhere.herokuapp.com';
-            const api=`${proxy}https://api.darksky.net/forecast/58526512f8707a0b2fd39b66ae32cf35/${lat},${long}`;
-        });
-
-        fetch(api)
-        .then(response =>{
-            return response.json();
-        })
-        .then(data =>{
-            console.log(data);
-            const {temperature, summary} =data.currently;
-
-
-        });
+    function setIcons(icon, iconId){
+        const skycons = new skycons({color: "white"});
+        const currentIcon = icon.replace(/-/g, "_");
     }
 });
